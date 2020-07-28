e49e99fdc7e01b081ab4ce5bbb677bd663e193a0
Raphael Coelho <raphael_coelho@hotmail.com.br>
2020-07-28 20:56:28 -0300

    #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="app">
-          <h1>MarioPlan</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     )
