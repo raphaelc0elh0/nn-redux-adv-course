330732998982f33e5414f420f5eb2a96f1ca2591
Raphael Coelho <raphael_coelho@hotmail.com.br>
2020-07-29 17:43:08 -0300

    #30

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,11 +1,13 @@
 export const createProject = (project) => {
   return (dispatch, getState, getFirebase) => {
     const firestore = getFirebase().firestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT', project: project })
